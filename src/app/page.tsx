0a2b413ffabfde30a0df7b52bbc6466bebb1557d
QuangNguyen <dongnguyen12122003@gmail.com>
2024-07-02 21:38:57 +1000

    finished assignment 1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ export default async function Home() {
   return (
     <main className={styles.main}>
       <div className={styles.container}>
-        <h1 className={styles.title}>Get Things Done</h1>
+        <h1 className={styles.title}>Get Things Done</h1> 
         <ul>
           <li> Task 1</li>
           <li> Task 2</li>
@@ -14,3 +14,5 @@ export default async function Home() {
     </main>
   );
 }
+
+// Finished Assignment 1
